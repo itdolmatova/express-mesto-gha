34acd797433588f7f99540909d63ab0e59070e96
Anastasya D <itdolmatova@gmail.com>
2022-08-25 01:14:59 +0300

    <refact>; joi validation

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
-    email: Joi.string().required(),
+    avatar: Joi.string().uri(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }).unknown(true),
 }), createUser);
